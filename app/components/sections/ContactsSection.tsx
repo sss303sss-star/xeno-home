5817d9a721de5cbd119e2ab2cc9c3b31eabdcac8
sss303sss-star <sss303sss@xenoimpact.com>
2026-05-22 10:37:33 +0900

    next/

--- app/components/sections/ContactsSection.tsx
+++ app/components/sections/ContactsSection.tsx
@@ -1,9 +1,6 @@
 "use client";
 
 import styles from "../../styles/sections/ContactsSection.module.css";
-import { BASE_PATH } from "../../lib/asset";
-
-const imgAlienIcon = `${BASE_PATH}/icons/alien.png`;
 
 const navLinks = ["Home", "About", "Experiences", "Products", "Customers"];
 
@@ -73,13 +70,15 @@ export default function ContactsSection() {
             </div>
             {/* Alien icon: 33×36 mobile / 46×50 md+ */}
             <div className="absolute right-0 top-[10px] w-[33px] h-[36px] md:w-[46px] md:h-[50px]">
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                src={imgAlienIcon}
-                alt=""
+              <svg
                 aria-hidden="true"
-                className="block size-full object-contain"
-              />
+                viewBox="0 0 46 50"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="block size-full"
+              >
+                <path d="M41.4496 9.10448C36.301 -0.0271368 23.0382 9.47433e-07 23.0382 9.47433e-07C23.0382 9.47433e-07 9.69305 -0.0271368 4.54446 9.10448C-0.164794 17.4627 -1.7437 27.1506 2.36145 33.9077C6.48033 40.6649 17.2992 50 23.0382 50C28.7772 50 39.5137 40.6649 43.6326 33.9077C47.7515 27.1506 46.1588 17.4627 41.4496 9.10448ZM20.6081 37.0149C20.1001 37.7069 14.7455 37.327 10.8601 34.4369C6.97459 31.5468 5.87623 27.5441 6.53525 26.54C7.19427 25.536 12.4527 26.1465 16.3519 29.0366C20.2374 31.9267 21.1435 36.3229 20.6218 37.0149H20.6081ZM35.134 34.4369C31.2485 37.327 25.894 37.7069 25.386 37.0149C24.878 36.3229 25.7704 31.9403 29.6559 29.0366C33.5414 26.1465 38.8135 25.5224 39.4725 26.54C40.1316 27.5577 39.0332 31.5468 35.1477 34.4369H35.134Z" fill="white"/>
+              </svg>
             </div>
           </div>
         </div>
